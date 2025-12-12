9c54ca2855d7641eb128b50fb1052311692aa029
Sagar Mitra <mitrasagar10@gmail.com>
2025-12-12 12:09:45 +0530

    Exploring HTTP methods

--- src/app.js
+++ src/app.js
@@ -4,14 +4,30 @@ const express = require("express");
 // Create a instance of the Express application
 const app = express();
 
+// This will only handle GET call to "/user"
+app.get("/user", (req, res) => {
+  res.send({ name: "Eren", age: 22, isLoggedIn: true });
+});
+
+// Only handle POST call to "/user"
+app.post("/user", (req, res) => {
+  res.send("Data successfully added to the database");
+});
+
+// Only handle DELETE call to "/user"
+app.delete("/user", (req, res) => {
+  res.send("User deleted successfully");
+});
+
+// This will match alll the HTTP method API calls to "/test"
+app.use("/test", (req, res) => {
+  res.send("This is the testing file.");
+});
+
 // Route for the route(/) url
 app.use("/", (req, res) => {
-    res.send("Hurrah app is running....")
-})
-
-app.use("/test", (req, res) => {
-    res.send("This is the testing file.")
-})
+  res.send("Hurrah app is running....");
+});
 
 // Start the server on port 3000
 // Once the server starts, the callback function runs
