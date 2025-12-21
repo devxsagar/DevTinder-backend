b4fae618367f64d57f2fa80e089a0047a86ac4a2
Sagar Mitra <mitrasagar10@gmail.com>
2025-12-21 08:14:31 +0530

    feat: add update user data and delete user from the database

--- src/app.js
+++ src/app.js
@@ -7,6 +7,61 @@ const app = express();
 // middleware
 app.use(express.json());
 
+// Get a user by ID
+app.get("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    //  const user = await User.findById({_id: userId});
+
+    // or we can write like this
+    const user = await User.findById(userId);
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (error) {
+    res.send("Something went wrong");
+  }
+});
+
+// Get all the user for feed
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.send("Something went wrong!!");
+  }
+});
+
+// Update user data
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+
+  try {
+    // await User.findByIdAndUpdate(userId, data); 
+
+    // This is also correct or we can write like above one
+    await User.findByIdAndUpdate({ _id: userId }, data);
+    res.send("User data updated succesfully");
+  } catch (error) {
+    res.send("Something went wrong!!");
+  }
+});
+
+// Delete a user
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  } catch (error) {
+    res.send("Something went wrong!!");
+  }
+});
+
 app.post("/signup", async (req, res) => {
   // Create a User Document
   const user = new User(req.body);
